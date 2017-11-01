281b6c293a99a1fcd4f32a5e1bf2764b4fe1eef8
liuximing <liuximing@szy.cn>
2017-11-01 16:08:48 +0800

    '提交关于webpack配置'

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
 	devtool: 'eval-source-map',
 	entry:  __dirname + "/app/js/main.js",//已多次提及的唯一入口文件
 	output: {
-		path: __dirname + "/public",//打包后的文件存放的地方
+		path: __dirname + "/build",//打包后的文件存放的地方
 		filename: "bundle.js"//打包后输出文件的文件名
 	},
 	devServer: {
@@ -34,9 +36,17 @@ module.exports = {
 						options: {
 							modules: true
 						}
+					}, {
+						loader: "postcss-loader"
 					}
 				]
 			}
 		]
-	}
+	},
+	plugins: [
+		new webpack.BannerPlugin('well版权所有，翻版必究'),
+		new HtmlWebpackPlugin({
+			template: __dirname + "/app/index.tmpl.html"//new 一个这个插件的实例，并传入相关的参数
+		})
+	],
 }
